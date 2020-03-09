596e45ca80f876552bb851bf77d7ef9de2850680
Tou Xiong <touyeexiong@gmail.com>
2020-03-09 13:22:54 -0500

    removed readme

---
+++ build/precache-manifest.63a49aec126846c482d90d63ed4ceb8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b163ab61f702c6e09bbbc2298fb962c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e71207d4d28cd554a406",
+    "url": "/static/css/2.1fd2b905.chunk.css"
+  },
+  {
+    "revision": "22c918caa4d81241116d",
+    "url": "/static/css/main.1a538199.chunk.css"
+  },
+  {
+    "revision": "e71207d4d28cd554a406",
+    "url": "/static/js/2.5f70b7f4.chunk.js"
+  },
+  {
+    "revision": "85ddfb896beafe699ec365171040d87b",
+    "url": "/static/js/2.5f70b7f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22c918caa4d81241116d",
+    "url": "/static/js/main.749845d4.chunk.js"
+  },
+  {
+    "revision": "0870f329a573baaaf1ca",
+    "url": "/static/js/runtime-main.2ad627c4.js"
+  }
+]);
